fcfe3cef4c23b5db5c3b33a61bd7a9b404023678
Pranay Bankar <pranay.bankar@konverge.ai>
2022-03-22 15:39:36 +0530

    complete question 2

--- hast_table_exercise.py
+++ hast_table_exercise.py
@@ -5,51 +5,20 @@ function to determine the index for storing or retrieving the data associated
 with a given key.
 """
 
-print("\n-----------Creat a Class to implement the hashing algorithm.-------------\n")
-
-"""
-Your objective in this assignment is to implement a HashTable class which supports 
-the following operations:
-
-Insert: Insert a new key-value pair
-Find: Find the value associated with a key
-Update: Update the value associated with a key
-List: List all the keys stored in the hash table
-
-"""
-
-class HashTable:
-
-    def insert(self, key, value):
-        """Insert a new key-value pair"""
-        pass
-    
-    def find(self, key):
-        """Find the value associated with a key"""
-        pass
-    
-    def update(self, key, value):
-        """Change the value associated with a key"""
-        pass
-    
-    def list_all(self):
-        """List all the keys"""
-        pass
-
 # As a first step is to create a Python list which will hold all the key-value pairs. 
 # We'll start by creating a list of a fixed size.
 
-# global variable
-MAX_HASH_TABLE_SIZE = 4096
-
 print('\n--QUESTION 1: Create a Python list of size `MAX_HASH_TABLE_SIZE`, \
     with all the values set to `None`.--\n')
+
+# global variable
+MAX_HASH_TABLE_SIZE = 4096
 
 hash_list = [None] * MAX_HASH_TABLE_SIZE
 
 print('len(hash_list) == MAX_HASH_TABLE_SIZE: ',len(hash_list) == MAX_HASH_TABLE_SIZE)
 
-print('\n---------------Impliment Hashing Function------------------\n')
+print('\n---------------QUESTION 2: Impliment Hashing Function------------------\n')
 
 """
 Algorithm for hashing, which can convert strings into numeric list indices.
@@ -88,3 +57,37 @@ print('\n-------List-------\n')
 # print(list(filter(lambda x: x is not None, hash_list)))
 pairs = [kv for kv in hash_list if kv is not None]
 print(pairs)    
+
+
+print("\n-----------QUESTION3: Creat a Class to implement the hashing algorithm.-------------\n")
+
+"""
+Your objective in this assignment is to implement a HashTable class which supports 
+the following operations:
+
+Insert: Insert a new key-value pair
+Find: Find the value associated with a key
+Update: Update the value associated with a key
+List: List all the keys stored in the hash table
+
+"""
+
+class HashTable:
+
+    def insert(self, key, value):
+        """Insert a new key-value pair"""
+        pass
+    
+    def find(self, key):
+        """Find the value associated with a key"""
+        pass
+    
+    def update(self, key, value):
+        """Change the value associated with a key"""
+        pass
+    
+    def list_all(self):
+        """List all the keys"""
+        pass
+
+
